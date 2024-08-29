0f4ec3276e7204d8b7f191b491449532c45cc0cb
Kha <buitranminhkha79120@gmail.com>
2024-08-29 20:01:15 +0700

    Đồ án Ngôn ngữ lập trình hiện đại

---
+++ Ban_Hang_The_Thao_/web_theThao/accounts/views.py
@@ -0,0 +1,95 @@
+﻿from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+from .forms import UserRegistrationForm, UserLoginForm, ManagerLoginForm, EditProfileForm
+from accounts.models import User
+
+
+def create_manager():
+    """
+    Tạo tài khoản admin
+    """
+    if not User.objects.filter(email="admin@gmail.com").first():
+        user = User.objects.create_user(
+            "admin@gmail.com", 'Admin shop' ,'123456'
+        )
+        # Cấp quyền admin
+        user.is_manager = True
+        user.save()
+
+
+def manager_login(request):
+    if request.method == 'POST':
+        form = ManagerLoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request, email=data['email'], password=data['password']
+            )
+            if user is not None and user.is_manager:
+                login(request, user)
+                return redirect('dashboard:products')
+            else:
+                messages.error(
+                    request, 'Tài khoản hoặc mật khẩu bị sai', 'danger'
+                )
+                return redirect('accounts:manager_login')
+    else:
+        form = ManagerLoginForm()
+    context = {'form': form}
+    return render(request, 'manager_login.html', context)
+
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = User.objects.create_user(
+                data['email'], data['full_name'], data['password']
+            )
+            return redirect('accounts:user_login')
+    else:
+        form = UserRegistrationForm()
+    context = {'title':'Signup', 'form':form}
+    return render(request, 'register.html', context)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request, email=data['email'], password=data['password']
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('shop:home_page')
+            else:
+                messages.error(
+                    request, 'Tài khoản hoặc mật khẩu bị sai', 'danger'
+                )
+                return redirect('accounts:user_login')
+    else:
+        form = UserLoginForm()
+    context = {'title':'Login', 'form': form}
+    return render(request, 'login.html', context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('accounts:user_login')
+
+
+def edit_profile(request):
+    form = EditProfileForm(request.POST, instance=request.user)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Hồ sơ của bạn đã được cập nhật', 'success')
+        return redirect('accounts:edit_profile')
+    else:
+        form = EditProfileForm(instance=request.user)
+    context = {'title':'Chỉnh sửa hồ sơ', 'form':form}
+    return render(request, 'edit_profile.html', context)
---
+++ Ban_Hang_The_Thao_/web_theThao/shop/views.py
@@ -0,0 +1,97 @@
+﻿from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.paginator import Paginator
+
+from shop.models import Product, Category
+from cart.forms import QuantityForm
+
+
+def paginat(request, list_objects):
+	p = Paginator(list_objects, 10)
+	page_number = request.GET.get('page')
+	try:
+		page_obj = p.get_page(page_number)
+	except PageNotAnInteger:
+		page_obj = p.page(1)
+	except EmptyPage:
+		page_obj = p.page(p.num_pages)
+	return page_obj
+
+
+def home_page(request):
+	products = Product.objects.all()
+	context = {'products': paginat(request ,products)}
+	return render(request, 'home_page.html', context)
+
+
+def product_detail(request, slug):
+	form = QuantityForm()
+	product = get_object_or_404(Product, slug=slug)
+	related_products = Product.objects.filter(category=product.category).all()[:5]
+	context = {
+		'title':product.title,
+		'product':product,
+		'form':form,
+		'favorites':'favorites',
+		'related_products':related_products
+	}
+	if request.user.likes.filter(id=product.id).first():
+		context['favorites'] = 'remove'
+	return render(request, 'product_detail.html', context)
+
+def about_us(request):
+
+    return render(request, 'aboutus.html', {})
+
+def blog_us(request):
+    return render(request, 'blog.html', {})	
+
+def contact_us(request):
+    return render(request, 'contact.html', {})	
+
+@login_required
+def add_to_favorites(request, product_id):
+	product = get_object_or_404(Product, id=product_id)
+	request.user.likes.add(product)
+	return redirect('shop:product_detail', slug=product.slug)
+
+
+@login_required
+def remove_from_favorites(request, product_id):
+	product = get_object_or_404(Product, id=product_id)
+	request.user.likes.remove(product)
+	return redirect('shop:favorites')
+
+
+@login_required
+def favorites(request):
+	products = request.user.likes.all()
+	context = {'title':'Favorites', 'products':products}
+	return render(request, 'favorites.html', context)
+
+
+def search(request):
+	query = request.GET.get('q')
+	products = Product.objects.filter(title__icontains=query).all()
+	context = {'products': paginat(request ,products)}
+	return render(request, 'home_page.html', context)
+
+
+def filter_by_category(request, slug):
+	"""Khi người dùng nhấp vào danh mục cha
+	ta muốn hiển thị tất cả sản phẩm trong các danh mục con của nó
+	"""
+	result = []
+	category = Category.objects.filter(slug=slug).first()
+	[result.append(product) \
+		for product in Product.objects.filter(category=category.id).all()]
+	# kiểm tra nếu danh mục là danh mục cha thì lấy tất cả các danh mục con
+	if not category.is_sub:
+		sub_categories = category.sub_categories.all()
+		# lấy tất cả sản phẩm trong các danh mục con
+		for category in sub_categories:
+			[result.append(product) \
+				for product in Product.objects.filter(category=category).all()]
+	context = {'products': paginat(request ,result)}
+	return render(request, 'home_page.html', context)
